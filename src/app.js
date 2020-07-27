808ada25189473b7357a9845ea7d99bc0891e412
Tiago da Cruz Santos <tiago.unity.cruz@gmail.com>
2020-07-26 22:29:04 -0300

    desafio md3

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,52 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title, url, techs} = request.body
+  const rep = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+  repositories.push(rep)
+  return response.json(rep)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {title, url, techs} = request.body
+  const repIndex = repositories.findIndex((element) => element.id == request.params.id)
+  if(repIndex < 0){
+    return response.status(400).json({msg: "rep not found"})
+  }
+  const rep = repositories[repIndex]
+  rep.title = title
+  rep.url = url
+  rep.techs = techs
+  return response.json(rep)
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const repIndex = repositories.findIndex((element) => element.id == request.params.id)
+  if(repIndex < 0){
+    return response.status(400).json({msg: "rep not found"})
+  }
+  repositories.splice(repIndex, 1)
+  return response.status(204).json({done: "done"})
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const repIndex = repositories.findIndex((element) => element.id == request.params.id)
+  if(repIndex < 0){
+    return response.status(400).json({msg: "rep not found"})
+  }
+  const rep = repositories[repIndex]
+  rep.likes += 1
+  return response.json(rep)
 });
 
 module.exports = app;
